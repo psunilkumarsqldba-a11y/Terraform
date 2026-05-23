813b6d96b9427ffc001c95dbc47246595980baff
psunilkumarsqldba-a11y <psunilkumarsqldba@gmail.com>
2026-05-23 16:34:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAU5SCSNVHNJEWJ2GM"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myvm" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t3.micro"
+  key_name = "Ubuntu"
+
+}
